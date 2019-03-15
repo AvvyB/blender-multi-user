5a831668c81184a2c19658bf90736b22d98efc22
Swann Martinez <s.martinez@cubedns.fr>
2019-03-15 18:24:20 +0100

    feat(rcf): initial ecs work

--- __init__.py
+++ __init__.py
@@ -18,14 +18,16 @@ import sys
 
 from . import net_operators
 from . import net_ui
+from . import net_ecs
 
 def register():
     bsyncio.register()
     net_operators.register()
     net_ui.register()
+    net_ecs.register()
 
 def unregister():
     bsyncio.unregister()
     net_ui.unregister()
     net_operators.unregister()
-    
+    net_ecs.unregister()
