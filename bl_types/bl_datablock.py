3b1aaa6fa5737458da670ff750cb271277c7908c
Swann <swann.martinez@protonmail.com>
2019-08-27 18:43:21 +0200

    feat: bump submodule

--- bl_types/bl_datablock.py
+++ bl_types/bl_datablock.py
@@ -8,7 +8,7 @@ from ..libs.replication.replication.data import ReplicatedDatablock
 class BlDatablock(ReplicatedDatablock):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        
+
         if self.pointer and hasattr(self.pointer, 'uuid'):
             self.pointer.uuid = self.uuid
 
