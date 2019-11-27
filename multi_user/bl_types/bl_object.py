dedc5a04e02176f054d40215f141d47b86cb089b
Swann Martinez <s.martinez@cubedns.fr>
2019-11-27 18:06:17 +0100

    feat: basic shape key support
    Related to #18

--- multi_user/bl_types/bl_object.py
+++ multi_user/bl_types/bl_object.py
@@ -72,15 +72,6 @@ class BlObject(BlDatablock):
         if "matrix_world" in data:
             target.matrix_world = mathutils.Matrix(data["matrix_world"])
 
-        # vertex groups
-        if 'vertex_groups' in data:
-            target.vertex_groups.clear()
-            for vg in data['vertex_groups']:
-                vertex_group = target.vertex_groups.new(name=vg['name'])
-                for vert in vg['vertices']:
-                    vertex_group.add(
-                        [vert['index']], vert['weight'], 'REPLACE')
-
         target.name = data["name"]
         # Load modifiers
         if hasattr(target, 'modifiers'):
@@ -124,6 +115,36 @@ class BlObject(BlDatablock):
         target.instance_type = data['instance_type']
         if data['instance_type'] == 'COLLECTION':
             target.instance_collection = bpy.data.collections[data['instance_collection']]
+
+        # vertex groups
+        if 'vertex_groups' in data:
+            target.vertex_groups.clear()
+            for vg in data['vertex_groups']:
+                vertex_group = target.vertex_groups.new(name=vg['name'])
+                for vert in vg['vertices']:
+                    vertex_group.add(
+                        [vert['index']], vert['weight'], 'REPLACE')
+
+        # SHAPE KEYS
+        if 'shape_keys' in data:
+            target.shape_key_clear()
+
+            object_data = target.data
+            
+            # Create keys and load vertices coords
+            for key_block in data['shape_keys']['key_blocks']:
+                key_data = data['shape_keys']['key_blocks'][key_block]
+                target.shape_key_add(name=key_block)
+                
+                utils.dump_anything.load(target.data.shape_keys.key_blocks[key_block],key_data)
+                for vert in key_data['data']:
+                    target.data.shape_keys.key_blocks[key_block].data[vert].co = key_data['data'][vert]['co']
+            
+            # Load relative key after all
+            for key_block in data['shape_keys']['key_blocks']:
+                reference = data['shape_keys']['key_blocks'][key_block]['relative_key']
+            
+                target.data.shape_keys.key_blocks[key_block].relative_key = target.data.shape_keys.key_blocks[reference]
 
     def dump_implementation(self, data, pointer=None):
         assert(pointer)
@@ -194,6 +215,35 @@ class BlObject(BlDatablock):
                 vg_data.append(dumped_vg)
 
             data['vertex_groups'] = vg_data
+        
+        #  SHAPE KEYS
+        pointer_data = pointer.data
+        if hasattr(pointer_data, 'shape_keys') and pointer_data.shape_keys:
+            dumper = utils.dump_anything.Dumper()
+            dumper.depth = 2
+            dumper.include_filter = [
+                'reference_key',
+                'use_relative'
+            ]
+            data['shape_keys'] = dumper.dump(pointer_data.shape_keys)
+            data['shape_keys']['reference_key'] = pointer_data.shape_keys.reference_key.name
+            key_blocks = {}
+            for key in pointer_data.shape_keys.key_blocks:
+                dumper.depth = 3
+                dumper.include_filter = [
+                    'name',
+                    'data',
+                    'mute',
+                    'value',
+                    'slider_min',
+                    'slider_max',
+                    'data',
+                    'co'
+                ]
+                key_blocks[key.name] = dumper.dump(key)
+                key_blocks[key.name]['relative_key'] = key.relative_key.name
+            data['shape_keys']['key_blocks'] = key_blocks
+
         return data
 
     def resolve(self):
