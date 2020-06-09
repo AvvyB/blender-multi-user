ad99a349f73ae05a990d72b872079a021daa0738
Swann <swann.martinez@protonmail.com>
2020-06-09 23:56:20 +0200

    feat: show admin status in UI feat: update submodule

--- multi_user/ui.py
+++ multi_user/ui.py
@@ -337,6 +337,7 @@ class SESSION_UL_users(bpy.types.UIList):
         ping = '-'
         frame_current = '-'
         scene_current = '-'
+        status_icon = 'NONE'
         if session:
             user = session.online_users.get(item.username)
             if user:
@@ -345,8 +346,10 @@ class SESSION_UL_users(bpy.types.UIList):
                 if metadata and 'frame_current' in metadata:
                     frame_current = str(metadata['frame_current'])
                     scene_current = metadata['scene_current']
+                if user['admin']:
+                    status_icon = 'FAKE_USER_ON'
         split = layout.split(factor=0.3)
-        split.label(text=item.username)
+        split.label(text=item.username, icon=status_icon)
         split = split.split(factor=0.5)
         split.label(text=scene_current)
         split.label(text=frame_current)
