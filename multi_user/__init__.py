53f11181813f2689a620a7fd70c840fd6d5ea2c0
Swann <swann.martinez@protonmail.com>
2020-07-27 17:44:27 +0200

    refactor: update download links

--- multi_user/__init__.py
+++ multi_user/__init__.py
@@ -21,7 +21,7 @@ bl_info = {
     "author": "Swann Martinez",
     "version": (0, 0, 3),
     "description": "Enable real-time collaborative workflow inside blender",
-    "blender": (2, 80, 0),
+    "blender": (2, 82, 0),
     "location": "3D View > Sidebar > Multi-User tab",
     "warning": "Unstable addon, use it at your own risks",
     "category": "Collaboration",
