dfa7f98126706bc972d4aa164ab5290aa88c6c21
Swann <swann.martinez@protonmail.com>
2020-10-05 11:28:45 +0200

    refactor: remove useless script

--- scripts/build_image.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-VERSION="$(python get_replication_version.py)"
-
-echo "Building docker image with version ${VERSION}"
-docker build --no-cache --build-arg version=${VERSION} -t registry.gitlab.com/slumber/multi-user/multi-user-server:0.1.0 ./docker_server
-
-echo "Pushing to gitlab registry ${VERSION}"
-docker push registry.gitlab.com/slumber/multi-user/multi-user-server:0.1.0 
