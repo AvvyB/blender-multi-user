f592294335396215b2fa72edf9967fe3db4f3c42
brybalicious <brybalicious@users.noreply.github.com>
2020-10-07 21:20:43 +0200

    Added scripts to conveniently start server instance via docker or replication

---
+++ scripts/start_server/run-dockerfile.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+# Start server in docker container, from image hosted on the multi-user gitlab's container registry
+docker run -d \
+    -p 5555-5560:5555-5560 \
+    -e port=5555 \
+    -e password=admin \
+    -e timeout=1000 \
+    registry.gitlab.com/slumber/multi-user/multi-user-server:0.1.0
