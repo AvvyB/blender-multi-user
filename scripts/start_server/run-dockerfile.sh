7892b5e9b6c7e1003561d9d3f5abbc6b5399223d
brybalicious <brybalicious@users.noreply.github.com>
2020-10-08 18:35:08 +0200

    Adding log-level to server startup scripts

--- scripts/start_server/run-dockerfile.sh
+++ scripts/start_server/run-dockerfile.sh
@@ -4,6 +4,7 @@
 docker run -d \
     -p 5555-5560:5555-5560 \
     -e port=5555 \
+    -e log-level DEBUG \
     -e password=admin \
     -e timeout=1000 \
     registry.gitlab.com/slumber/multi-user/multi-user-server:0.1.0
