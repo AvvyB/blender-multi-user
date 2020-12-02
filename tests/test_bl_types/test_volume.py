49fcc40db9ce896671bc6d7b86c4b0e6dea464cd
Swann <swann.martinez@protonmail.com>
2020-12-02 22:36:30 +0100

    feat: add a bsic volume test

---
+++ tests/test_bl_types/test_volume.py
@@ -0,0 +1,21 @@
+import os
+
+import pytest
+from deepdiff import DeepDiff
+
+import bpy
+import random
+from multi_user.bl_types.bl_volume import BlVolume
+
+def test_volume(clear_blend):
+    datablock = bpy.data.volumes.new("Test")
+
+    implementation = BlVolume()
+    expected = implementation._dump(datablock)
+    bpy.data.volumes.remove(datablock)
+
+    test = implementation._construct(expected)
+    implementation._load(expected, test)
+    result = implementation._dump(test)
+
+    assert not DeepDiff(expected, result)
