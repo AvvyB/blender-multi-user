a4ef8a6344d300e47466618dab3f772fbe8b23d8
Swann <swann.martinez@protonmail.com>
2021-01-15 23:14:45 +0100

    feat: skip update in sculpt mode by default

--- multi_user/bl_types/bl_mesh.py
+++ multi_user/bl_types/bl_mesh.py
@@ -126,7 +126,7 @@ class BlMesh(BlDatablock):
     def _dump_implementation(self, data, instance=None):
         assert(instance)
 
-        if instance.is_editmode and not self.preferences.sync_flags.sync_during_editmode:
+        if (instance.is_editmode or bpy.context.mode == "SCULPT") and not self.preferences.sync_flags.sync_during_editmode:
             raise ContextError("Mesh is in edit mode")
         mesh = instance
 
