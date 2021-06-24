f33c3d8481a08dd648a1225d33b50336d229aee9
Swann <swann.martinez@protonmail.com>
2021-06-24 14:50:12 +0200

    fix: doc version

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Swann Martinez'
 author = 'Swann Martinez, with contributions from Poochy'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.0'
+release = '0.5.0-develop'
 
 
 # -- General configuration ---------------------------------------------------
